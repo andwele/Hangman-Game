2072f5d591271d9e40a194f027972904523a9709
Michael Andwele Hill <michaelandwelehill@Michaels-MacBook-Air.local>
2017-05-27 03:16:57 -0400

    commit1

---
+++ assets/javascript/game.js
@@ -0,0 +1,80 @@
+
+// Movie List.
+var movies = [
+    "dumbo",
+    "bambi", 
+    "peter pan",
+    "cinderella",
+    "the lady and the tramp",
+    "101 Dalmations", 
+    "the aristocats",
+    "the rescuers", 
+    "winnie the pooh",
+    "the fox and the hound",
+    "beauty and the beast", 
+    "alladin", 
+    "the jungle book", 
+    "the santa clause", 
+    "the lion king", 
+    "snow white and the seven dwarfs",
+    "heavyweights", 
+    "pocahontas", 
+    "a goofy movie", 
+    "toy story", 
+    "the mighty ducks",
+    "the hunchback of notre dame", 
+    "james and the giant peach", 
+    "air bud", 
+    "george of the jungle",
+    "hercules",
+    "flubber", 
+    "mulan",
+    "mighty joe young", 
+    "a bugs life", 
+    "johnny tsunami", 
+    "tarzan",
+    "dinosaur", 
+    "the emperors new groove",
+    "atlants the lost empire",
+    "the lady and the tramp",
+    "monsters inc",
+    "lilo and stitch",
+    "treasure planet", 
+    "brother bear",
+    "holes", 
+    "pirates of the carribean", 
+    "finding nemo", 
+    "the incredibles",
+    "around the world in eighty days", 
+    "national treasure",
+    "sky high", 
+    "the chronicles of narnia",
+    "the proud family movie",
+    "chicken little",
+    "bambi", 
+    "enchanted", 
+    "ratatouille", 
+    "wall e",
+    "the princess and the frog"
+];
+
+
+    // Press any key to get started 
+
+    // Wins and Losses.
+    function score () {
+        
+    }
+
+    // Letters already guessed
+
+    // Number of guesses remaining
+
+    // Current Word
+
+    // Music Playing in the background
+
+
+
+var numberOfSpaces = movies[i].toLowerCase().replace(" ", "").length; 
+var guessesAvailable = numberOfSpaces + 4;
